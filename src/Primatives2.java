2ea18daef3ed87a626ce4640947fc2ddc2c5825d
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-05 12:57:09 -0400

    Update

--- src/Primatives2.java
+++ src/Primatives2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Primatives2 {
+public class Primatives2 {
+    public static void main(String[] args) {
+
+        byte myMinByteValue = -128;
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        float myFloatValue = 23.467F;
+        float myFloatValue2 = 1.4e-3f;
+
+        System.out.println(myFloatValue);
+        System.out.println(myFloatValue2);
+
+        double myDoubleValue = -98.467F;
+        double myDoubleValue2 = 1.8e-3f;
+
+        System.out.println(myDoubleValue);
+        System.out.println(myDoubleValue2);
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+    }
 }
